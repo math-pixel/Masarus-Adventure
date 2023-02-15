80ad4115507dba0c03868b264c4db71c6713d349
P-Lrou <pl74rou@gmail.com>
2023-02-15 12:49:41 +0100

    add toolsFunction for collisions

--- src/Tools.js
+++ src/Tools.js
@@ -58,3 +58,64 @@ function findIndexOfPositionIn2dArray(posX,posY,array,sideArrayX,sideArrayY){
     }
     return previous_index_pos;
 }
+
+// Tout fonctionne avec un tableau pour exprimer un rectangle sous cette forme -> [x, y, largeur, hauteur]
+
+// Get Corners
+// Prends un rect et renvoie un point sous forme -> [x, y]
+function getTopCornerLeft(rect) {
+    return [rect[0], rect[1]]
+}
+
+function getTopCornerRight(rect) {
+    return [rect[0] + rect[2], rect[1]]
+}
+
+function getBottomCornerLeft(rect) {
+    return [rect[0], rect[1] + rect[3]]
+}
+
+function getBottomCornerRight(rect) {
+    return [rect[0] + rect[2], rect[1] + rect[3]]
+}
+
+// Point is in rect
+// Prend un point et un rect et renvoie true ou false
+function pointIsInRect(point, rect) {
+    if (point[0] > rect[0] && point[0] < (rect[0] + rect[2]) && point[1] > rect[1] && (rect[1] + rect[3])) {
+        return true
+    }
+    return false
+}
+
+// rect is in rect
+// prend deux rect et renvoie un tableau avec les points qui sont en contact avec le deuxième rectangle
+function rectIsInRect(rect1, rect2) {
+    let tabOfCorner = [];
+    if (pointIsInRect(getTopCornerLeft(rect1[0], rect1[1], rect1[2], rect1[3]), rect2)) {
+        tabOfCorner.push("TopCornerLeft")
+    }
+    if (pointIsInRect(getTopCornerRight(rect1[0], rect1[1], rect1[2], rect1[3]), rect2)) {
+        tabOfCorner.push("TopCornerRight")
+    }
+    if (pointIsInRect(getBottomCornerLeft(rect1[0], rect1[1], rect1[2], rect1[3]), rect2)) {
+        tabOfCorner.push("BottomCornerLeft")
+    }
+    if (pointIsInRect(getBottomCornerRight(rect1[0], rect1[1], rect1[2], rect1[3]), rect2)) {
+        tabOfCorner.push("BottomCornerRight")
+    }
+    return tabOfCorner
+}
+
+// expandRect
+// prend un rect et une taille et renvoie le rectangle agrandi avec la taille souhaiter
+function expandRect(rect, expandSize) {
+    return [rect[0] - expandSize, rect[1] - expandSize, rect[2] + expandSize, rect[3] + expandSize]
+}
+
+
+// ShrinkRect
+// prend un rect et une taille et renvoie le rectangle rétrécie avec la taille souhaiter
+function shrinkRect(rect, skrinkSize) {
+    return [rect[0] + skrinkSize, rect[1] + skrinkSize, rect[2] - (skrinkSize*2), rect[3] - skrinkSize]
+}
