2449dbdcd9181eb06e8b80d5473fe9820195e6fc
math-pixel <mathcb74@gmail.com>
2023-03-11 22:10:09 +0100

    update engine 2

--- src/engine2/engine2.js
+++ src/engine2/engine2.js
@@ -1,3 +1,7 @@
 function startEngine2(){
-    drawMapEngine2(MapPipeGame.Map.layers)
+    setRandomRotation(MapPipeGame.Map.layers);
+    drawMapEngine2(MapPipeGame.Map.layers);
+
+    fill(255,150,0)
+    rect(0,0,30,20)
 }
